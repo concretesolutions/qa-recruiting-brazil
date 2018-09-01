b9793413acac1edeed30bd221997abdad14431ba
rfranca86 <universius@gmail.com>
2018-09-01 14:11:46 -0300

    Desafio concrete

---
+++ features/steps/envio_foto_video_whatsappweb_steps.py
@@ -0,0 +1,35 @@
+from behave import then, when, given
+
+@given(u'que o usuário está logado no WhatsAppWeb')
+def step_impl(context):
+    pass
+
+
+@given(u'inicia uma conversa com o Morty')
+def step_impl(context):
+    pass
+
+
+@when(u'abre a janela de envio de arquivos')
+def step_impl(context):
+    pass
+
+
+@when(u'seleciona a imagem "pickle_rick.jpeg"')
+def step_impl(context):
+    pass
+
+
+@then(u'a imagem é enviada')
+def step_impl(context):
+    pass
+
+
+@then(u'seleciona o video "GOT_s08_teaser.avi"')
+def step_impl(context):
+    pass
+
+
+@then(u'o video é enviado')
+def step_impl(context):
+    pass
---
+++ features/steps/envio_mensagem_whatsappweb_steps.py
@@ -0,0 +1,29 @@
+from behave import then, when, given
+
+@given(u'que o usuario está logado no WhatsAppWeb')
+def step_impl(context):
+    pass
+
+
+@given(u'abre o chat com o Morty')
+def step_impl(context):
+    pass
+
+@when(u'envia uma mensagem')
+def step_impl(context):
+    pass
+
+
+@then(u'o contato recebeu a mensagem')
+def step_impl(context):
+    pass
+
+
+@then(u'inicia uma conversa do grupo X-Wing')
+def step_impl(context):
+    pass
+
+
+@then(u'o grupo recebeu a mensagem')
+def step_impl(context):
+    pass
