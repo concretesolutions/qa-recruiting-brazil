3ec1411e6eae038e47102832ffa7249a3e4a0c0f
BiankaMoises <52638594+BiankaMoises@users.noreply.github.com>
2019-07-08 22:53:13 -0300

    Add files via upload

---
+++ WhatsApp/src/test/java/Test/Runner/GlueDesabilitarSom.java
@@ -0,0 +1,17 @@
+package Test.Runner;
+
+
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber; 
+ 
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/java/Test/Runner", glue="") 
+
+
+
+
+public class GlueDesabilitarSom {
+
+}
