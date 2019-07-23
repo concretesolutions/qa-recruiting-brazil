9febba96081e955f0793035551865279dc60414e
hearcha <38765193+hearcha@users.noreply.github.com>
2019-07-23 11:02:54 -0300

    Add files via upload

---
+++ teste.rb
@@ -0,0 +1,39 @@
+require 'test/unit'
+require_relative 'Checkout.rb'
+
+class TestPrice < Test::Unit::TestCase
+
+  def price(goods)
+    co = CheckOut.new(RULES)
+    goods.split#(//).each { |item| co.scan(item) }
+    co.total
+  end
+
+  def test_totals
+    assert_equal(  0, price(" "))
+    assert_equal( 50, price("A"))
+    assert_equal( 80, price("AB"))
+    assert_equal(115, price("CDBA"))
+
+    assert_equal(100, price("AA"))
+    assert_equal(130, price("AAA"))
+    assert_equal(180, price("AAAA"))
+    assert_equal(230, price("AAAAA"))
+    assert_equal(260, price("AAAAAA"))
+
+    assert_equal(160, price("AAAB"))
+    assert_equal(175, price("AAABB"))
+    assert_equal(190, price("AAABBD"))
+    assert_equal(190, price("DABABA"))
+  end
+
+  def test_incremental
+    co = CheckOut.new(RULES)
+    #assert_equal(  0, co.total)
+    co.scan("A");  assert_equal( 50, co.total)
+    co.scan("B");  assert_equal( 80, co.total)
+    co.scan("A");  assert_equal(130, co.total)
+    co.scan("A");  assert_equal(160, co.total)
+    co.scan("B");  assert_equal(175, co.total)
+  end
+end
