79681f625a82e26eaf78a94c86379a1fed21ac1b
Robson Mendes <binhoconvites@gmail.com>
2019-08-01 00:45:05 -0300

    Update, Desafio 09 resolvido e cenários de funcionalidades do Whatsapp
    Algoritmo do Desafio 09 resolvido em Vb.NET e cenários de teste em arquivo docx
    
    Nome: Robson Mendes
    e-mail: robsonmendes.dev@gmail.com

---
+++ Desafio_09/Desafio_09/Desafio_09.vb
@@ -0,0 +1,101 @@
+﻿'Nome: Robson Mendes de Lima Filho
+'e-mail: robsonmendes.dev@gmail.com
+
+'Algoritmo desenvolvido em Vb.NET
+Module Module1
+
+    'Tabela problema
+    'A 50 3 por 130 terceiro sai por 30 ***** 3º com 40% de desconto ***** será cobrado 60% do valor
+    'B 30 2 por 45  segundo  sai por 15 ***** 2º com 50% de desconto ***** será cobrado 50% do valor
+    'C 20
+    'D 15
+    Sub Main()
+
+        Dim vQtd_A, vQtd_B, vQtd_C, vQtd_D, vBarato_A, vBarato_B As Integer
+        Dim vValor_A, vValor_B, vValor_C, vValor_D, vValor_Final As Double
+        Dim vTotal_Produtos, vFechar_Caixa As String
+
+        'Conforme tabela, o mercado possui 4 produtos, de forma que os valores dos mesmos não devem ser fixos
+        Console.WriteLine("SUPER MERCADO: Produtos de A a D !!!")
+        Console.WriteLine("_____________________________________________________________________")
+        Console.Write("Digite o valor de venda do produto A: ")
+        vValor_A = Console.ReadLine
+
+        Console.Write("Digite o valor de venda do produto B: ")
+        vValor_B = Console.ReadLine
+
+        Console.Write("Digite o valor de venda do produto C: ")
+        vValor_C = Console.ReadLine
+
+        Console.Write("Digite o valor de venda do produto D: ")
+        vValor_D = Console.ReadLine
+        Console.WriteLine("_____________________________________________________________________")
+
+        vFechar_Caixa = "N"
+
+        While vFechar_Caixa = "N"
+
+            'Produtos devem ser digitados pelos nomes A,B,C e/ou D... ex: (aaabdda)
+            Console.WriteLine("")
+
+            Console.Write("Digite os produtos a serem comprados: ")
+            vTotal_Produtos = Console.ReadLine
+            vTotal_Produtos = UCase(vTotal_Produtos)
+
+            'Contar Produtos 
+            Dim vConta_Total_Produtos = 0
+            For i = 1 To 4
+                vConta_Total_Produtos = vConta_Total_Produtos + 1
+                If vConta_Total_Produtos = 1 Then
+                    Dim vTotal_A = vTotal_Produtos
+                    vTotal_A = vTotal_A.Replace("B", "")
+                    vTotal_A = vTotal_A.Replace("C", "")
+                    vTotal_A = vTotal_A.Replace("D", "")
+                    vQtd_A = Len(vTotal_A)
+
+                ElseIf vConta_Total_Produtos = 2 Then
+                    Dim vTotal_B = vTotal_Produtos
+                    vTotal_B = vTotal_B.Replace("A", "")
+                    vTotal_B = vTotal_B.Replace("C", "")
+                    vTotal_B = vTotal_B.Replace("D", "")
+                    vQtd_B = Len(vTotal_B)
+
+                ElseIf vConta_Total_Produtos = 3 Then
+                    Dim vTotal_C = vTotal_Produtos
+                    vTotal_C = vTotal_C.Replace("A", "")
+                    vTotal_C = vTotal_C.Replace("B", "")
+                    vTotal_C = vTotal_C.Replace("D", "")
+                    vQtd_C = Len(vTotal_C)
+
+                ElseIf vConta_Total_Produtos = 4 Then
+                    Dim vTotal_D = vTotal_Produtos
+                    vTotal_D = vTotal_D.Replace("A", "")
+                    vTotal_D = vTotal_D.Replace("B", "")
+                    vTotal_D = vTotal_D.Replace("C", "")
+                    vQtd_D = Len(vTotal_D)
+                End If
+            Next
+
+            'Quantidade de produtos com desconto
+            vBarato_A = Math.Floor(vQtd_A / 3)
+            vBarato_B = Math.Floor(vQtd_B / 2)
+            'Quantidade de produtos sem desconto
+            vQtd_A = vQtd_A - vBarato_A
+            vQtd_B = vQtd_B - vBarato_B
+
+            'Cálculo do valor a ser cobrado
+            vValor_Final = (vQtd_A * vValor_A) + (vQtd_B * vValor_B) + (vQtd_C * vValor_C) + (vQtd_D * vValor_D) +
+                       (vBarato_A * (0.6 * vValor_A)) + (vBarato_B * (0.5 * vValor_B))
+
+            Console.WriteLine("")
+            Console.Write("VALOR TOTAL A COBRAR: " & vValor_Final)
+            Console.WriteLine("")
+
+            Console.Write("Deseja fechar o caixa ?  Digite 'N' para continuar ou 'S' para encerrar. ")
+            vFechar_Caixa = Console.ReadLine
+            vFechar_Caixa = UCase(vFechar_Caixa)
+        End While
+
+    End Sub
+
+End Module
---
+++ Desafio_09/Desafio_09/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associada a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("Desafio_09")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Desafio_09")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("ecc42576-4e10-4acb-ad95-07a0241d38fb")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+' É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+' utilizando o "*" como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
