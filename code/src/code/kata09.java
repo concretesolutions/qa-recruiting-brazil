7117cc732856218dcd1f1c93d76145ef3223c45e
David Hazan <david_shazan95@hotmail.com>
2019-10-15 20:04:02 -0300

    ENH- Changed project to eclipse template

---
+++ code/src/code/kata09.java
@@ -0,0 +1,9 @@
+package code;
+
+public class kata09 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ code/src/tests/junit.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class junit {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
