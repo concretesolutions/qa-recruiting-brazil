714c934b9f42775532a4952d7e9e7488d6389e8b
Renato Araújo <renatojoa@gmail.com>
2020-08-10 18:37:47 -0300

    feat(checkout-kata): incremental test and refactor

--- checkout/checkout.py
+++ checkout/checkout.py
@@ -3,15 +3,11 @@ from rules import prices
 from copy import deepcopy
 
 class Checkout():
-
+      
   def __init__(self):
     self.copy_prices = deepcopy(prices)
     self.total = 0
 
-  @staticmethod
-  def new(newRules):
-    return Checkout()
-    
   def scan(self,sku):
     self.count(sku)    
     if self.has_discount(sku):
@@ -34,34 +30,21 @@ class Checkout():
   def count(self, sku):
     if self.copy_prices[sku]['repeat'] == 0:
       self.copy_prices[sku]['repeat'] = 1
-      
     elif(self.copy_prices[sku]['repeat'] == 0 and self.copy_prices[sku]['specialPrice'] == 0):
       self.copy_prices[sku]['repeat'] = 0
     else:
       self.copy_prices[sku]['repeat'] += 1
 
-class TestStringMethods(unittest.TestCase):
   def scanSKU(self,skus):
-    c = Checkout.new(prices)
+    self.total = 0
+    self.copy_prices = deepcopy(prices)
     for sku in skus:
-      c.scan(sku)
-    print(c.total)
-    return c.total
+      self.scan(sku.upper())
+    print(self.total)
+    return self.total
 
-  def test_totals(self):
-    self.assertEqual(self.scanSKU(''), 0 )
-    self.assertEqual(self.scanSKU('A'), 50)
-    self.assertEqual(self.scanSKU('AB'), 80)
-    self.assertEqual(self.scanSKU('CDBA'), 115)
-    self.assertEqual(self.scanSKU('AA'), 100)
-    self.assertEqual(self.scanSKU('AAA'), 130)
-    self.assertEqual(self.scanSKU('AAAA'), 180)
-    self.assertEqual(self.scanSKU('AAAAA'), 230)
-    self.assertEqual(self.scanSKU('AAAAAA'), 260)
-    self.assertEqual(self.scanSKU('AAAB'), 160)
-    self.assertEqual(self.scanSKU('AAABB'), 175)
-    self.assertEqual(self.scanSKU('AAABBD'), 190)
-    self.assertEqual(self.scanSKU('DABABA'), 190)
-
-if __name__ == '__main__':
-  unittest.main()
+  def scanUnit(self,skus):
+    for sku in skus:
+      self.scan(sku.upper())
+    return self.total	
+ 
---
+++ runTest.py
@@ -0,0 +1,5 @@
+import unittest
+from tests.test import *
+ 
+if __name__ == '__main__':
+    unittest.main()
