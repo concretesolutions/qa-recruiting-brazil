9bfb662b3312e2ad96686a4d4de08d04afb61cad
Bruno Pulis <bruno.pulis@monetizze.com.br>
2020-11-05 01:58:26 -0300

    test(checkout-a): adiciona teste para checkout com item A

--- src/checkout.js
+++ src/checkout.js
@@ -1,14 +1,15 @@
 class Checkout {
   constructor(rules) {
-
+    this.rules = rules;
+    this.amountValue = 0;
   }
 
   scan(item) {
-
+    this.amountValue += this.rules.find(i => i.item === item).price;
   }
 
   total() {
-    return 0;
+    return this.amountValue;
   }
 }
 
--- src/checkout.spec.js
+++ src/checkout.spec.js
@@ -1,12 +1,31 @@
 import Checkout from './checkout'
 
-const pricingRules = [];
+const rules = [
+  {
+    "item": "A",
+    "price": 50
+  }
+];
 
 describe('Checkout', () => {
   describe('Test total', () => {
-    it('should be return 0 to empty checkout', () => {
-      const checkout = new Checkout(pricingRules);
+  it('should be return 0 to empty checkout', () => {
+      // Arrange
+      const checkout = new Checkout(rules);
+
+      // Assert
       expect(checkout.total()).toBe(0);
-    });    
+    });
+    
+  it("should be return 50 if the item was 'A'", () => {
+      // Arrange
+      const checkout = new Checkout(rules);
+
+      // Act
+      checkout.scan("A");
+
+      // Assert
+      expect(checkout.total()).toBe(50);
+    })
   });
 });
